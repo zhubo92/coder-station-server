49a635c0487aa0469fc766dac3a2bca2191a356c
朱波 <zhubo0327@gmail.com>
2024-11-06 10:49:51 +0800

    fix: db

--- db/connect.js
+++ db/connect.js
@@ -4,17 +4,13 @@
 
 const mongoose = require("mongoose");
 
-const {DB_USER_NAME, DB_PASSWORD, DB_HOST, DB_NAME} = process.env;
-
 // 定义链接数据库字符串
-const dbURI = `mongodb://${DB_USER_NAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+const dbURI = `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
 
 // 连接
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  console.log(`连接失败还是成功？`);
-});
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // 监听
 mongoose.connection.on("connected", function () {
-  console.log(`${DB_NAME}数据库已经连接...`);
+  console.log(`${process.env.DB_NAME}数据库已连接`);
 });
